34ee435080c486a499ea0ea1fe72d8e6b62f371f
jokade <jokade@karchedon.de>
2018-08-06 19:03:57 +0200

    Update for scalanative-cocoa 0.0.1

--- src/main/g8/src/main/scala/AppDelegate.scala
+++ src/main/g8/src/main/scala/AppDelegate.scala
@@ -1,40 +1,23 @@
 import cocoa.foundation._
-import cocoa.uikit.{UIApplication, UIApplicationDelegate, UIResponder}
+import cocoa.uikit.{UIApplication, UIApplicationDelegate}
 import de.surfice.smacrotools.debug
-import objc.ScalaObjC
+
+import scalanative.native._
 
 @ScalaObjC
 @debug
-class AppDelegate(self: NSObject) extends UIResponder with UIApplicationDelegate {
+class AppDelegate(self: NSObject) extends NSObject with UIApplicationDelegate {
 
-  override def application(application: UIApplication, didFinishLaunchingWithOptions: NSDictionary[NSObject,NSObject]): BOOL = {
+  def application(application: UIApplication, didFinishLaunchingWithOptions: NSDictionary[NSObject,NSObject]): BOOL = {
     true
   }
 
-  //  private var _clickCount = 0
-//
   /* Outlets */
-//  var window: NSObject = _
   private var _window: NSObject = _
   def window: NSObject = _window
   def window_=(w: NSObject): Unit = {
     w.retain()
     _window = w
   }
-//  var clickCountView: NSTextField = _
-//
-//  /* Actions */
-//  def takeClick(id: NSObject): Unit = {
-//    _clickCount += 1
-//    updateView()
-//  }
-//
-//  override def applicationDidFinishLaunching(notification: NSNotification): Unit = {
-//    updateView()
-//  }
-//
-//  private def updateView(): Unit = {
-//    clickCountView.setIntegerValue(_clickCount)
-//  }
 }
 
--- src/main/g8/src/main/scala/ViewController.scala
+++ src/main/g8/src/main/scala/ViewController.scala
@@ -1,16 +1,20 @@
 import cocoa.foundation._
-import cocoa.uikit.{UIViewController, UIViewControllerClass}
+import cocoa.uikit._
 import de.surfice.smacrotools.debug
-import objc.ScalaObjC
+import scalanative.native._
 
 @ScalaObjC
 class ViewController(self: UIViewController) extends UIViewController {
-  override def viewDidLoad(): Unit = {
-    Foundation.NSLog(ns"loading")
-    \$super(self)(_.viewDidLoad())
-  }
-}
+  var labelView: UILabel = _
 
-//object ViewController extends UIViewControllerClass {
-//  override type InstanceType = UIViewController
-//}
+  /* Actions */
+  def takeClick(id: NSObject): Unit = {
+    labelView.setText(ns"Hello, Scala Native!")
+  }
+
+  override def viewDidLoad(): Unit = {
+    \$super(self)(_.viewDidLoad())
+    labelView.setText(ns"" )
+  }
+  
+}
